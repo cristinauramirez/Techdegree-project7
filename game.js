2e43b03af684e8060e7069e789cc765cae984a31
Cristina Ramirez <cristina.u.ramirez@gmail.com>
2019-02-07 22:25:06 -0700

    created a phrase array

--- game.js
+++ game.js
@@ -6,6 +6,14 @@ const startGame = document.querySelector('a');
 
 let missed = 0;
 
+let phrases = [
+  'Never Walk Alone',
+  'Spring Day',
+  'Serendipity',
+  'So Fat Away',
+  'Night Drive'
+];
+
 //start game event listener
 startGame.addEventListener('click', (e) => {
   overlay.style.display = 'none';
