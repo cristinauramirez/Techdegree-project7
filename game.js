db36cb051d45c618744df1bbbae7ffbb5f0148c7
Cristina Ramirez <cristina.u.ramirez@gmail.com>
2019-02-07 22:23:17 -0700

    added a event listener to hide screen overlay

--- game.js
+++ game.js
@@ -2,4 +2,11 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 
+const startGame = document.querySelector('a');
+
 let missed = 0;
+
+//start game event listener
+startGame.addEventListener('click', (e) => {
+  overlay.style.display = 'none';
+});
